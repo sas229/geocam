eec1f178422fa95ca6bc00decc5a9da23234fed2
Hilario Greggi <hilario210199@gmail.com>
2023-04-27 18:31:15 +0100

    Created methods for the Controller Class - pseudo code

--- src/geocam/controller.py
+++ src/geocam/controller.py
@@ -3,6 +3,60 @@ class Controller:
     def __init__(self):
         print("Created a Controller instance.")
 
+    def check_available_network_interfaces():
+        """
+        - Should check the type of network - and if a connection to internet is available or not
+        - Raise the necessary error if need be
+        """
+
+        return
+    
+    def choose_the_rig():
+        """
+        - Should allow the user to choose one of the rigs in the lab - which triaxial 
+        - Ideally, since the number of rigs and there number of pis/cameras will be known, it could ba a good idea to set these infos here 
+        """
+        return 
+
+    def identify_the_pis():
+        """
+        - Create a connection with all the pis allocated to a rig
+        - Check the number of pis known for the rig 
+        - Raise the necessary error if need be  
+        """
+        return
+    
+    def configure_cameras(): 
+        """
+        - Configuration and contols of the camera, must dive in the picamera2-mannual for the pi cameras 
+        - Would be different rules for other cameras - we could make a child class to control each type of cameras that'll be used. Configure would then be specific to each camera 
+        - To be reflected on 
+        """
+        return
+    
+    def focus():
+        """
+        - Start the focus process
+        - Calls functions from the calibration module 
+        """ 
+        return
+
+    def calibrate(): 
+        """
+        - Get the calibration data for each camera
+        - Call the Calibration module 
+        """
+        return 
+    
+    def aquire_images():
+        """
+        - Start taking the pictures 
+        - The way to retrieve them should be discussed
+        """
+        return
+    
+    
+    
 
 if __name__ == "__main__": 
     test = Controller()
