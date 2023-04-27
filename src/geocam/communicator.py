ebbf4013dafcf942f352eb8c8e2b08c9856056b0
Hilario Greggi <hilario210199@gmail.com>
2023-04-27 19:47:39 +0100

    Proposition to use child/parent classes in the communicator module

--- src/geocam/communicator.py
+++ src/geocam/communicator.py
@@ -1,6 +1,6 @@
 class Communicator(): 
 
-    def __init__(self): 
+    def __init__(self, device:str = 'controller',  mcast_grp:str = '224.1.1.1', mcast_port:int = 9998, tcp_port:int = 47822): 
         print("Created a Communicator instance.")
 
     def tcp_link(self): 
@@ -22,12 +22,6 @@ class Communicator():
         - Send a ping on the lan
         """
         return
-    
-    def stream(self): 
-        """
-        - Stream video to a ip address
-        """
-        return
         
     def _create_json(self): 
         """
@@ -35,4 +29,35 @@ class Communicator():
         """
         return 
     
+#############################################################################################################################################
     
+class Sender(Communicator):
+    """
+    - They should only be one type of sender: computers. 
+
+    Args:
+        Communicator (_type_): _description_
+    """
+    def __init__(self, device: str = 'controller', mcast_grp: str = '224.1.1.1', mcast_port: int = 9998, tcp_port: int = 47822):
+        super().__init__(device, mcast_grp, mcast_port, tcp_port)
+
+class Receiver(Communicator): 
+    """_summary_
+
+    Args:
+        Communicator (_type_): _description_
+    """
+    def __init__(self, device: str = 'controller', mcast_grp: str = '224.1.1.1', mcast_port: int = 9998, tcp_port: int = 47822):
+        super().__init__(device, mcast_grp, mcast_port, tcp_port)
+
+##############################################################################################################################################
+    
+class ListeningRPi(Receiver): 
+    def __init__(self, device: str = 'controller', mcast_grp: str = '224.1.1.1', mcast_port: int = 9998, tcp_port: int = 47822):
+        super().__init__(device, mcast_grp, mcast_port, tcp_port)
+
+    def stream(self): 
+        """
+        - Stream video to a ip address
+        """
+        return
