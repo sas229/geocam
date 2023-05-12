7daaef56c11262b4e17662b150c49035bef44921
Hilario Greggi <hilario210199@gmail.com>
2023-05-12 10:32:22 +0100

    First try to strem with pyshine

--- src/geocam/rpicamera.py
+++ src/geocam/rpicamera.py
@@ -26,6 +26,8 @@ import socket
 import time 
 if platform.system() == "Linux":
     from picamera2 import Picamera2
+    import cv2
+    import  pyshine as ps
 
 from geocam.communicator import *
 from geocam.utils import *
@@ -36,6 +38,19 @@ from geocam.utils import *
 #############################################################################################################################################
 
 class RPicamera(): 
+
+    HTML="""
+    <html>
+    <head>
+    <title>PyShine Live Streaming</title>
+    </head>
+
+    <body>
+    <center><h1> PyShine Live Streaming using OpenCV </h1></center>
+    <center><img src="stream.mjpg" width='640' height='480' autoplay playsinline></center>
+    </body>
+    </html>
+    """
 
     def __init__(self) -> None: 
         print("Created a RPicamera instance.")
@@ -63,7 +78,30 @@ class RPicamera():
                     event_time = time.time()
                     time_past = event_time - start_time
                     print(f"exited after {time_past} seconds. timeout was set to {timeout} seconds")
-                
+    
+    def stream(self):
+        StreamProps = ps.StreamProps
+        StreamProps.set_Page(StreamProps,self.HTML)
+        host_ip = self.id_info["ip_addr"]
+        address = (host_ip,9000) # Enter your IP address for host_ip
+        try:
+            StreamProps.set_Mode(StreamProps,'cv2')
+            capture = cv2.VideoCapture(0)
+            capture.set(cv2.CAP_PROP_BUFFERSIZE,4)
+            capture.set(cv2.CAP_PROP_FRAME_WIDTH,320)
+            capture.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
+            capture.set(cv2.CAP_PROP_FPS,30)
+            StreamProps.set_Capture(StreamProps,capture)
+            StreamProps.set_Quality(StreamProps,90)
+            server = ps.Streamer(address,StreamProps)
+            print('Server started at','http://'+address[0]+':'+str(address[1]))
+            server.serve_forever()
+            
+        except KeyboardInterrupt:
+            capture.release()
+            server.socket.close()
+        
+                    
     
     def excecute(self, request:dict, socket_tcp:CustomSocket = None, addr:str = None, print_info:bool = True) -> None:
         
@@ -108,9 +146,9 @@ class RPicamera():
             self.collaborator.send(message, sock_tcp=socket_tcp, target_ip=addr[0], info_sent = False)
 
         # CALIBRATE
-        if command == "calibrate":
+        if command == "stream":
             print(f"{command} job")
-            pass 
+            self.stream() 
 
         # CONFIGURE CAMERAS 
         if command == "configure_cameras":
