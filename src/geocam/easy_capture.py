c1f8ade62b07312d4f21fe0d1a0380427eb2fc2f
Hilario Greggi <hilario210199@gmail.com>
2023-05-23 15:47:25 +0100

    correct easy_capture.py file

--- src/geocam/easy_capture.py
+++ src/geocam/easy_capture.py
@@ -33,8 +33,14 @@ logger.addHandler(stream_handler)
 #############################################################################################################################################
 
 # Create a queue to store the image buffers
-image_queue = queue.Queue()
+data_queue = queue.Queue()
 max_queue_size = 10
+
+# Create a lock for data 
+data_lock = threading.Lock()
+
+# Creating the buffer variable. None initially to have a clean plate
+data_buffer = None
 
 # Step 1: Define the stop flag
 stop_thread = False
@@ -44,21 +50,32 @@ server_address = ('172.20.10.2', 1645)
 sock.connect(server_address)
 
 def send_images():
-    logger.info("sleeping*****")
-    time.sleep(5)
-    logger.info("done sleeping*****")
+    global data_queue
+    global stop_thread
+    global sock
+    
+    logger.info("THIS IS THE VALUE OF STOP THREAD %s", stop_thread)
+    # logger.info("sleeping*****")
+    # time.sleep(5)
+    # logger.info("done sleeping*****")
     while True: 
-        if not image_queue.empty():
+
+        if not data_queue.empty():
             logger.info("QUEUE")
-            data = image_queue.get()  # Retrieve buffer from image_queue
+            data = data_queue.get()  # Retrieve buffer from data_queue
             logger.info("CHECK1")
             image_size = data.getbuffer().nbytes
+            logger.info("image_size %s", image_size)
             logger.info("CHECK2")
-            logger.info("THIS IS IMAGE SIZE %s", image_size)
-            sock.sendall(struct.pack('!I', image_size))
+            logger.info("THIS IS THE SIZE OF THE DATA %s", len(struct.pack('!I', image_size)))
+            logger.info("SENDALL RETURNS %s", sock.sendall(struct.pack('!I', image_size)))
             logger.info("CHECK3")
             sock.sendall(data.getvalue())
             logger.info("CHECK4")
+            # Close the data_copy object to release resources
+            data.close()
+            logger.info("THIS IS THE VALUE OF STOP THREAD NOW %s", stop_thread)
+
         if stop_thread:
             logger.info("BREAK")
             break
@@ -70,7 +87,8 @@ class GeoPicamera(Picamera2):
         logger.debug(self.__str__())
 
     def start_capture_and_send_images(self, name: str = "image{:03d}.jpg", initial_delay=1, num_files=1, delay=1, show_preview=False):
-        data = io.BytesIO()
+        global data_buffer
+        global data_queue
 
         # the two following will be useful for more detailed configuration 
         preview_config = self.create_preview_configuration()
@@ -79,37 +97,49 @@ class GeoPicamera(Picamera2):
         if self.started:
             self.stop()
 
-        if delay:
-            # Show a preview between captures, so we will switch mode and back for each capture.
-            self.configure(preview_config)
-            self.start(show_preview=show_preview)
-            
-            for i in range(num_files):
-                time.sleep(initial_delay if i == 0 else delay)
-                # self.switch_mode_and_capture_file(capture_mode, name.format(i))
-                self.switch_mode_and_capture_file(capture_config, data, format='jpeg')
-                image_queue.put(data)
-                if image_queue.qsize() >= max_queue_size:
-                    break
+        with data_lock:
+            logger.info("IN LOCK")
 
-        else:
-            # No preview between captures, it's more efficient just to stay in capture mode.
-            if initial_delay:
+            data_buffer = io.BytesIO()
+
+            if delay:
+                # Show a preview between captures, so we will switch mode and back for each capture.
                 self.configure(preview_config)
                 self.start(show_preview=show_preview)
-                time.sleep(initial_delay)
-                self.switch_mode(capture_config)
                 
-            else:
-                self.configure(capture_config)
-                self.start(show_preview=show_preview)
+                for i in range(num_files):
+                    time.sleep(initial_delay if i == 0 else delay)
+                    # self.switch_mode_and_capture_file(capture_mode, name.format(i))
+                    self.switch_mode_and_capture_file(capture_config, data_buffer, format='jpeg')
+                    data_copy = io.BytesIO(data_buffer.getvalue())
+                    data_queue.put(data_copy)
 
-            for i in range(num_files):
-                self.capture_file(data, format='jpeg')
-                image_queue.put(data)
-                if i == num_files - 1 or image_queue.qsize >= max_queue_size:
-                    break
-                time.sleep(delay)
+            else:
+                # No preview between captures, it's more efficient just to stay in capture mode.
+                if initial_delay:
+                    self.configure(preview_config)
+                    self.start(show_preview=show_preview)
+                    time.sleep(initial_delay)
+                    self.switch_mode(capture_config)
+                    
+                else:
+                    self.configure(capture_config)
+                    self.start(show_preview=show_preview)
+
+                for i in range(num_files):
+                    self.capture_file(data_buffer, format='jpeg')
+                    data_copy = io.BytesIO(data_buffer.getvalue())
+                    data_queue.put(data_copy)
+                    
+                    if i == num_files - 1:
+                        break
+                    time.sleep(delay)
+
+            # Reset the data buffer for the next order 
+            data_buffer = None
+            logger.info("the queue is not empty %s", not data_queue.empty())
+            logger.info("OUT OF LOCK")
+
         self.stop()
 
 
@@ -120,10 +150,11 @@ if __name__ == '__main__':
     sending_thread.start()
 
     camera = GeoPicamera()
-    camera.start_capture_and_send_images(initial_delay=0, delay=0, num_files=1)
+    camera.start_capture_and_send_images(initial_delay=1, delay=0.5, num_files=2)
 
     stop_thread = True
-    sock.close()
     sending_thread.join()
+    logger.info("CHANGING THE STOP THREAD")
+    sock.close()
 
     
