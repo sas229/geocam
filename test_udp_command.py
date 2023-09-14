942345be4d33a9058d7df1b8d2beb3273f7173b0
sas229 <sas229@cam.ac.uk>
2023-09-14 13:46:00 +0100

    Tidied up erroneous files.

--- test_udp_command.py
+++
@@ -1,6 +0,0 @@
-import socket
-import json
-
-sendsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-command = {"command": "captureFrame"}
-sendsock.sendto(bytes(json.dumps(command), "utf-8"), ("192.168.161.232", 3179))
