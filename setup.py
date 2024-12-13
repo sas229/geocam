c7793bfe20a89122f6e8d992db93e2846d764da4
Sam Stanier <sas229@cam.ac.uk>
2024-12-13 11:47:26 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     packages = ["geocam", "geocam.dependencies"],
+    packages=find_packages(where="src"),
     package_dir={"": "src"},
     package_data={"geocam.dependencies":['*'],},
     entry_points={
         "console_scripts": [
-            "run-server=geocam.backend.server:main",
+            "run-server=backend.server:main",
         ],
     },
 )
