dcd1707c624e9eb800578097a7ff1b2753017735
Sam Stanier <sas229@cam.ac.uk>
2024-12-13 12:10:47 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ setup(
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    package_data={"geocam.dependencies":['*'],},
+    package_data={
+        "geocam.dependencies":['*'],
+        "frontend":['*'],
+    },
     entry_points={
         "console_scripts": [
             "geocam-server=backend.server:run",
