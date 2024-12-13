bc6db09f28856549ad7158b2527e776deb6d3ed5
Sam Stanier <sas229@cam.ac.uk>
2024-12-13 12:57:10 +0000

    Removed unecessary MANIFEST.in file.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="geocam",
-    version="0.1.0",
+    version="0.0.7",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     package_data={
